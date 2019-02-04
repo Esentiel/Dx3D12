3a4818cb36fb0d2882cf03026483e871652c2a2f
Artem Bursuk <Artembu@intern.blackwood.gg>
2019-02-04 11:43:04 +0200

    lightning wave added

---
+++ D3D12lib/Headers/LightWaves.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include "d3dApp.h"
+#include "MathHelper.h"
+#include "UploadBuffer.h"
+#include "GeometryGenerator.h"
+#include "FrameResource.h"
+#include "MeshGeometry.h"
+#include "Waves.h"
+#include <unordered_map>
+#include <DirectXMath.h>
+
+struct RenderItem;
+
+using Microsoft::WRL::ComPtr;
+
+enum class RenderLayer : int
+{
+	Opaque = 0,
+	Count
+};
+
+class LitWavesApp : public D3DApp
+{
+public:
+	LitWavesApp(HINSTANCE hInstance);
+	LitWavesApp(const LitWavesApp& rhs) = delete;
+	LitWavesApp& operator=(const LitWavesApp& rhs) = delete;
+	~LitWavesApp();
+
+	virtual bool Initialize()override;
+
+private:
+	virtual void OnResize()override;
+	virtual void Update(const GameTimer& gt)override;
+	virtual void Draw(const GameTimer& gt)override;
+
+	virtual void OnMouseDown(WPARAM btnState, int x, int y)override;
+	virtual void OnMouseUp(WPARAM btnState, int x, int y)override;
+	virtual void OnMouseMove(WPARAM btnState, int x, int y)override;
+
+	void OnKeyboardInput(const GameTimer& gt);
+	void UpdateCamera(const GameTimer& gt);
+	void UpdateObjectCBs(const GameTimer& gt);
+	void UpdateMaterialCBs(const GameTimer& gt);
+	void UpdateMainPassCB(const GameTimer& gt);
+	void UpdateWaves(const GameTimer& gt);
+
+	void BuildRootSignature();
+	void BuildShadersAndInputLayout();
+	void BuildLandGeometry();
+	void BuildWavesGeometryBuffers();
+	void BuildPSOs();
+	void BuildFrameResources();
+	void BuildMaterials();
+	void BuildRenderItems();
+	void DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems);
+
+	float GetHillsHeight(float x, float z)const;
+	DirectX::XMFLOAT3 GetHillsNormal(float x, float z)const;
+
+private:
+
+	std::vector<std::unique_ptr<FrameResource>> mFrameResources;
+	FrameResource* mCurrFrameResource = nullptr;
+	int mCurrFrameResourceIndex = 0;
+
+	UINT mCbvSrvDescriptorSize = 0;
+
+	ComPtr<ID3D12RootSignature> mRootSignature = nullptr;
+
+	std::unordered_map<std::string, std::unique_ptr<MeshGeometry>> mGeometries;
+	std::unordered_map<std::string, std::unique_ptr<Material>> mMaterials;
+	std::unordered_map<std::string, std::unique_ptr<Texture>> mTextures;
+	std::unordered_map<std::string, ComPtr<ID3DBlob>> mShaders;
+	std::unordered_map<std::string, ComPtr<ID3D12PipelineState>> mPSOs;
+
+	std::vector<D3D12_INPUT_ELEMENT_DESC> mInputLayout;
+
+	RenderItem* mWavesRitem = nullptr;
+
+	// List of all the render items.
+	std::vector<std::unique_ptr<RenderItem>> mAllRitems;
+
+	// Render items divided by PSO.
+	std::vector<RenderItem*> mRitemLayer[(int)RenderLayer::Count];
+
+	std::unique_ptr<Waves> mWaves;
+
+	PassConstants mMainPassCB;
+
+	DirectX::XMFLOAT3 mEyePos = { 0.0f, 0.0f, 0.0f };
+	DirectX::XMFLOAT4X4 mView = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 mProj = MathHelper::Identity4x4();
+
+	float mTheta = 1.5f*DirectX::XM_PI;
+	float mPhi = DirectX::XM_PIDIV2 - 0.1f;
+	float mRadius = 50.0f;
+
+	float mSunTheta = 1.25f*DirectX::XM_PI;
+	float mSunPhi = DirectX::XM_PIDIV4;
+
+	POINT mLastMousePos;
+};
--- D3D12lib/Main.cpp
+++ D3D12lib/Main.cpp
@@ -4,7 +4,8 @@
 #include "Utils.h"
 //#include "BoxApp.h"
 //#include "ShapesApp.h"
-#include "LandAndWavesApp.h"
+//#include "LandAndWavesApp.h"
+#include "LightWaves.h"
 
 // simple window
 //int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
@@ -82,6 +83,31 @@
 //}
 
 // waves and land
+//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+//	PSTR cmdLine, int showCmd)
+//{
+//	// Enable run-time memory check for debug builds.
+//#if defined(DEBUG) | defined(_DEBUG)
+//	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+//#endif
+//
+//	try
+//	{
+//		LandAndWavesApp theApp(hInstance);
+//		if (!theApp.Initialize())
+//			return 0;
+//
+//		return theApp.Run();
+//	}
+//	catch (DxException& e)
+//	{
+//		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+//		return 0;
+//	}
+//}
+
+
+// light waves
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 	PSTR cmdLine, int showCmd)
 {
@@ -92,7 +118,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 
 	try
 	{
-		LandAndWavesApp theApp(hInstance);
+		LitWavesApp theApp(hInstance);
 		if (!theApp.Initialize())
 			return 0;
 
